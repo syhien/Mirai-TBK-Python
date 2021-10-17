b8dc51c08d63d1b97c34582d86606c2f4804b302
syhien <syhien@outlook.at>
2021-10-17 14:02:42 +0800

    更准确的搜索（应该），支持优惠券链接

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     find_req = top.api.TbkDgMaterialOptionalRequest()
     find_req.set_app_info(top.appinfo('33082016', 'df6564cdf0c69b64581f915c4b770324'))
     find_req.adzone_id = '111713900272'
-    find_limit = 1
+    find_req.sort = 'match_des'
 
     ring_req = top.api.TbkTpwdCreateRequest()
     ring_req.set_app_info(top.appinfo('33082016', 'df6564cdf0c69b64581f915c4b770324'))
@@ -24,18 +24,24 @@ if __name__ == '__main__':
             return
         if (not (re.search('[0-9a-zA-Z]{11}', str(event.message_chain)) and len(str(event.message_chain)) > 11)) and (At(bot.qq) not in event.message_chain):
             return
-        find_req.q = re.sub(u"([^\u4e00-\u9fa5])", "", str(event.message_chain))
+        if At(bot.qq) in event.message_chain:
+            find_req.q = str(event.message_chain[Plain])
+        else:
+            find_req.q = re.sub(u"([^\u4e00-\u9fa5])", "", str(event.message_chain[Plain]))
         try:
             cnt = 0
             for i in find_req.getResponse()['tbk_dg_material_optional_response']['result_list']['map_data']:
                 # await bot.send(event, i['title'] + ' https:' + i['url'])
                 try:
-                    ring_req.url = 'https:' + i['url']
+                    if 'coupon_share_url' in i:
+                        ring_req.url = 'https:' + i['coupon_share_url']
+                    else:
+                        ring_req.url = 'https:' + i['url']
                     await bot.send(event, ring_req.getResponse()['tbk_tpwd_create_response']['data']['model'], quote=True)
                 except Exception as e:
                     return bot.send(event, str(e))
                 cnt += 1
-                if cnt == find_limit:
+                if cnt == 2:
                     break
             return
         except Exception as e:
